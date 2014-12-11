b6f183f62188ae82bd9187d6c6da5a7e651ecfef
Eric Brelsford <ebrelsford@gmail.com>
2014-12-11 12:40:25 -0500

    Proper name for MANIFEST, include all python files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-twitter-feed',
-    version='0.2',
+    version='0.2.2',
     packages=['twitter_feed'],
     include_package_data=True,
     license='MIT License',
